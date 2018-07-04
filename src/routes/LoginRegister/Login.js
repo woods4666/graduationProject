1cb96c2f872b8ecbd314357f17846356cc49143f
woods4666 <1219257166@qq.com>
2018-07-04 12:21:30 +0800

    搞定登录注册的样式

--- src/routes/LoginRegister/Login.js
+++ src/routes/LoginRegister/Login.js
@@ -1,14 +1,70 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+
+const FormItem = Form.Item;
+
 class Login extends React.Component {
     constructor(props,context){
         super(props,context);
     }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
     render(){
-        return <div>
-            
+        const { getFieldDecorator } = this.props.form;
+        return <div className="loginBox">
+            <div className="title">老用户登录</div>
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <FormItem>
+                    {getFieldDecorator('userName', {
+                        rules: [{ required: true, message: 'Please input your username!' }],
+                    })(
+                        <Input placeholder="请输入手机号" />
+                    )}
+                </FormItem>
+                <div className="password">
+                    <FormItem>
+                        {getFieldDecorator('password', {
+                            rules: [{ required: true, message: 'Please input your Password!' }],
+                        })(
+                            <Input type="password" placeholder="请输入密码" />
+
+                        )}
+                    </FormItem>
+                    <a className="login-form-forgot forgot" href="">忘记密码</a>
+                </div>
+
+                <FormItem>
+                    {/*{getFieldDecorator('remember', {
+                        valuePropName: 'checked',
+                        initialValue: true,
+                    })(
+                        <Checkbox>Remember me</Checkbox>
+                    )}*/}
+
+                    <a href="" className="phoneLogin">
+                        <span>手机号快捷登录</span>
+                        <Icon type="right"/>
+                    </a>
+                    <a href="#">
+                        <Button type="primary" htmlType="submit" className="login-form-button green">
+                            登录
+                        </Button>
+                    </a>
+
+                    <a href=""><Button className='gray'>
+                        注册
+                    </Button></a>
+                </FormItem>
+            </Form>
         </div>
     }
 }
 
-export default connect()(Login)
+export default Form.create()(connect()(Login));
--- src/routes/LoginRegisterBase.js
+++ src/routes/LoginRegisterBase.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Icon} from 'antd'
+import Login from './LoginRegister/Login'
+import Register from './LoginRegister/Register'
 import '../static/css/loginRegister.less'
 class LoginRegisterBase extends React.Component {
     constructor(props, context) {
@@ -8,10 +10,16 @@ class LoginRegisterBase extends React.Component {
     }
 
     render() {
+        console.log(this.props.location);
+        let {pathname}=this.props.location;
         return <div className="loginRegisterBox">
+
             <div className="loginHeader">
                 <Icon type="left-circle-o" />
                 <img src={require('../static/images/loginLogo.png')} alt=""/>
+            </div>
+            <div className="loginBody">
+                {pathname==='/login'?<Login/>:<Register/>}
             </div>
         </div>
     }
